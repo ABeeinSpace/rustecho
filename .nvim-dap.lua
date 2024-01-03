b596c55c0cbf4970fad34c349bcaf4d4e97ae892
Aidan Border <planes4u@outlook.com>
2024-01-03 10:11:39 -0500

    Project initialized with cargo inint
    Also added nvim-dap project configuration for debugging support

---
+++ .nvim-dap.lua
@@ -0,0 +1,25 @@
+local dap = require("dap")
+
+codelldb_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb"
+dap.adapters.lldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+        command = codelldb_path,
+        args = { "--port", "${port}" },
+        detached = false,
+    }
+}
+
+dap.configurations.rust = {
+    {
+        name = "factorial",
+        type = "lldb",
+        request = "launch",
+        program = function()
+            return vim.fn.getcwd() .. "/target/debug/factorial"
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+    },
+}
