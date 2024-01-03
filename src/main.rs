5434a18e705e56898f22be09095410d5a1d3cb63
Aidan Border <planes4u@outlook.com>
2024-01-03 10:37:15 -0500

    Working on integrating the clap crate and defining marguments for use in main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+use std::{io, u8};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
